d85f96e52ef89d2e3f8cffdd321384e5420bd87a
brlrb <bikramcode@gmail.com>
2017-04-01 19:22:57 -0700

    added rating functionality

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -55,13 +55,11 @@ import UIKit
     private func setupButtons() {
         
         // Clear any existing buttons
-        for button in ratingButtons{
+        for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
         }
-        
         ratingButtons.removeAll()
-        
         
         // Load Button Images
         let bundle = Bundle(for: type(of: self))
@@ -69,18 +67,15 @@ import UIKit
         let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
         let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
         
-        
-        for _ in 0..<starCount {
-        
-            // Create a button
+        for index in 0..<starCount {
+            // Create the button
             let button = UIButton()
+            
             // Set the button images
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
             button.setImage(highlightedStar, for: .highlighted)
             button.setImage(highlightedStar, for: [.highlighted, .selected])
-            
-            
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -88,15 +83,15 @@ import UIKit
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // Setup the button action
-            button.addTarget(self, action:#selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-            
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
             // Add the button to the stack
             addArrangedSubview(button)
             
-            // Add the new button to the raiting button array
+            // Add the new button to the rating button array
             ratingButtons.append(button)
-        
         }
+        
+        updateButtonSelectionStates()
     }
 }
