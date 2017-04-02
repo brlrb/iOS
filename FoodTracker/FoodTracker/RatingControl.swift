986996cbef71d29d289bc055bfc6498f2ebc9675
brlrb <bikramcode@gmail.com>
2017-04-01 18:57:02 -0700

    Added rating height/wigth

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -17,6 +17,19 @@ import UIKit
     var rating = 0
     
     
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height:44.0){
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    
+    @IBInspectable var starCount: Int = 5{
+        didSet{
+            setupButtons()
+        }
+    }
+    
 
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -41,7 +54,15 @@ import UIKit
     //MARK: Private Methods
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // Clear any existing buttons
+        for button in ratingButtons{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
         
             // Create a button
             let button = UIButton()
@@ -49,8 +70,8 @@ import UIKit
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // Setup the button action
             button.addTarget(self, action:#selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
